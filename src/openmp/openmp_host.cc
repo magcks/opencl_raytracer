49ddf190fd7aad4381b76f35e454d72e2596f63d
Max von Buelow <max.von.buelow@gris.informatik.tu-darmstadt.de>
2018-01-17 18:34:18 +0100

    removed openmp version, as the intel opencl lib can be used to run on the CPU

--- src/openmp/openmp_host.cc
+++
@@ -1,8 +0,0 @@
-#include "openmp_host.h"
-
-
-bool OpenMPHost::operator()(Mesh &mesh, std::vector<unsigned int> &nodes, std::vector<Vec3f> &aabbs, float * tmp) {
-	this->kernel.iterate(mesh, nodes, aabbs, tmp);
-
-	return true; // TODO
-}
