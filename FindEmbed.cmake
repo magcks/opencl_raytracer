dcb98ddeeea4ae95e6ec6f3c798a1057ed8a6ec6
Max von Buelow <max@m9x.de>
2018-01-22 20:20:06 +0100

    Updated embed

--- FindEmbed.cmake
+++ FindEmbed.cmake
@@ -1,7 +1,8 @@
 # - Provide a macro to embed binary files into the executable.
-# The module defines the following variables:
 #
-#  The module defines the macros:
+# This file is part of the Embed project: https://github.com/magcks/embed
+#
+# The module defines the macros:
 #
 #  EMBED_TARGET(<Name> <BinFile>)
 #
@@ -11,14 +12,14 @@
 # The macro defines a set of variables:
 #  EMBED_${Name}_DEFINED       - true is the macro ran successfully
 #  EMBED_${Name}_INPUT         - The input source file, an alias for <BinFile>
-#  EMBED_${Name}_OUTPUT        - The source file generated
+#  EMBED_${Name}_OUTPUTS        - The source file generated
 #
 #  ====================================================================
 #  Example:
 #
-#   find_package(Embed)
-#   EMBED_TARGET(mysource source.glsl)
-#   add_executable(Foo main.cc ${EMBED_mysource_OUTPUTS})
+#   find_package(Embed REQUIRED)
+#   EMBED_TARGET(SHADER source.glsl)
+#   add_executable(example main.cc ${EMBED_SHADER_OUTPUTS})
 #  ====================================================================
 
 
@@ -32,7 +33,7 @@ macro(EMBED_TARGET Name Input)
 	if(WIN32)
 		set(OutputRC "${CMAKE_CURRENT_BINARY_DIR}/${Name}.rc")
 		set(OutputC "${CMAKE_CURRENT_BINARY_DIR}/${Name}.c")
-		set(Output ${OutputRC} ${OutputC})
+		set(Outputs ${OutputRC} ${OutputC})
 		set(RCCODE "#define ${Name} ${RESID}\n${Name} RCDATA \"${InputAbs}\"\n")
 		set(CODE "#include \"windows.h\"\n${STRUCT} struct Res ${Name}() { HMODULE handle = GetModuleHandle(NULL)\; HRSRC res = FindResource(handle, MAKEINTRESOURCE(${RESID}), RT_RCDATA)\; struct Res r = { (const char*)LockResource(LoadResource(handle, res)), SizeofResource(handle, res) }\; return r\; }\n")
 		file(WRITE ${OutputRC} ${RCCODE})
@@ -45,16 +46,17 @@ macro(EMBED_TARGET Name Input)
 			set(Section ".section .rodata")
 		endif()
 		set(CODE "asm(\"${Section}\\n.align ${CMAKE_SIZEOF_VOID_P}\\ndata: .incbin \\\"${InputAbs}\\\"\\nenddata:\\n\")\; ${STRUCT} extern const char data[]\; extern const char enddata[]\; struct Res ${Name}() { struct Res r = { data, enddata - data }\; return r\; }\n")
-		set(Output "${CMAKE_CURRENT_BINARY_DIR}/${Name}.c")
-		file(WRITE ${Output} ${CODE})
+		set(OutputC "${CMAKE_CURRENT_BINARY_DIR}/${Name}.c")
+		set(Outputs ${OutputC})
+		file(WRITE ${OutputC} ${CODE})
 
 		add_custom_command(
-			OUTPUT ${Output}
-			COMMAND ${CMAKE_COMMAND} -E touch ${Output}
+			OUTPUT ${OutputC}
+			COMMAND ${CMAKE_COMMAND} -E touch ${OutputC}
 			DEPENDS ${Input}
 		)
 	endif()
 	set(EMBED_${Name}_DEFINED TRUE)
 	set(EMBED_${Name}_INPUT ${Input})
-	set(EMBED_${Name}_OUTPUT ${Output})
+	set(EMBED_${Name}_OUTPUTS ${Outputs})
 endmacro()
